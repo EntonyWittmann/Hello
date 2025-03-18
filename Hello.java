852e6a063c52bf8470a98337a2cf27b20af57969
EntonyWittmann <entonywittmann@gmail.com>
2025-03-18 19:58:30 -0300

    segundo commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        portugues();
+    }
+    public static void portugues() {
+        System.out.println("Ola Mundo!");
     }
 }
